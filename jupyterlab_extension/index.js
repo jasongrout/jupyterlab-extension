f1db6bde4f58fa26378ad1a1a6ff5106fd447d15
Jason Grout <jgrout6@bloomberg.net>
2016-06-02 12:31:53 -0400

    Update server extension to JupyterLab 0.12.0

--- jupyterlab_extension/index.js
+++ jupyterlab_extension/index.js
@@ -2,20 +2,19 @@
 // Distributed under the terms of the Modified BSD License.
 'use strict';
 
-// Load CSS.
-require('font-awesome/css/font-awesome.min.css');
-require('jupyter-js-plugins/lib/default-theme/index.css');
+var phosphide = require('phosphide/lib/core/application');
 
 // ES6 Promise polyfill
 require('es6-promise').polyfill();
 
-var phosphide = require('phosphide/lib/core/application');
+require('font-awesome/css/font-awesome.min.css');
+require('jupyter-js-plugins/lib/default-theme/index.css');
 
 var app = new phosphide.Application({
   extensions: [
     require('phosphide/lib/extensions/commandpalette').commandPaletteExtension,
     require('jupyter-js-plugins/lib/terminal/plugin').terminalExtension,
-    require('jupyter-js-plugins/lib/filehandler/plugin').fileHandlerExtension,
+    require('jupyter-js-plugins/lib/editorhandler/plugin').editorHandlerExtension,
     require('jupyter-js-plugins/lib/filebrowser/plugin').fileBrowserExtension,
     require('jupyter-js-plugins/lib/imagehandler/plugin').imageHandlerExtension,
     require('jupyter-js-plugins/lib/help/plugin').helpHandlerExtension,
@@ -23,15 +22,18 @@ var app = new phosphide.Application({
     require('jupyter-js-plugins/lib/shortcuts/plugin').shortcutsExtension,
     require('jupyter-js-plugins/lib/about/plugin').aboutExtension,
     require('jupyter-js-plugins/lib/landing/plugin').landingExtension,
+    require('jupyter-js-plugins/lib/console/plugin').consoleExtension,
     require('jupyter-js-plugins/lib/main/plugin').mainExtension,
   ],
   providers: [
-    require('jupyter-js-plugins/lib/filehandler/plugin').fileHandlerProvider,
+    require('jupyter-js-plugins/lib/clipboard/plugin').clipboardProvider,
+    require('jupyter-js-plugins/lib/docregistry/plugin').docRegistryProvider,
     require('jupyter-js-plugins/lib/services/plugin').servicesProvider,
-    require('jupyter-js-plugins/lib/rendermime/plugin').renderMimeProvider
+    require('jupyter-js-plugins/lib/rendermime/plugin').renderMimeProvider,
+    require('jupyter-js-plugins/lib/notebook/plugin').activeNotebookProvider
   ]
 });
 
 window.onload = function() {
-  app.run();
+    app.run();
 }
